263314172b77921c8cf0b7bcb053a051930988f5
Sachin Singh <100368589+Sachin2815@users.noreply.github.com>
2023-08-02 10:48:50 +0530

    Update runninginstance.py

--- runninginstance.py
+++ runninginstance.py
@@ -1,1 +1,20 @@
+import boto3
+def get_running_instance_count():
+    try:
+        
+        ec2_client = boto3.client('ec2')
+        response = ec2_client.describe_instances()
+        running_instance_count = 0
+        for reservation in response['Reservations']:
+            for instance in reservation['Instances']:
+                if instance['State']['Name'] == 'running':
+                    running_instance_count += 1
+        return running_instance_count
 
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return None
+
+running_instances = get_running_instance_count()
+if running_instances is not None:
+    print(f"Number of running instances: {running_instances}")
